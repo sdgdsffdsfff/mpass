6799b4afa0dd2fbd80549dfa7c6e3f5aecf8b728
chenyf <rstevens2008@hotmail.com>
2015-03-03 18:20:04 +0800

    up

--- router/conf.sh
+++ router/conf.sh
@@ -10,12 +10,10 @@ export LOG_DIR=/letv/logs/mpass/$AGENT_NAME
 export RUN_DIR=/letv/run/mpass/$AGENT_NAME
 
 export LOG_FILE=$LOG_DIR/agent.log
-export WORK_DIR=$RUN_DIR/work
-export STATUS_DIR=$RUN_DIR/status
 export LOCK_FILE=$RUN_DIR/agent.lock
 export LOG_TYPE="file"
 
-export THIS_HOST=10.154.156.122
+export THIS_HOST=$(grep IPADDR /etc/sysconfig/network-scripts/ifcfg-eth0 | awk 'BEGIN{FS="="} {print $2}')
 
 ## running mode: rdtest, qatest, product
 RUNMODE="product"
