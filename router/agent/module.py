ebcfd787f4f48cda7d15267e1fe90074592d45ac
chenyf <rstevens2008@hotmail.com>
2015-03-04 21:03:50 +0800

    up

--- router/agent/module.py
+++ router/agent/module.py
@@ -28,7 +28,23 @@ server {
         proxy_set_header X-Real-IP $remote_addr;
     }
 }"""
-        self.tpl = Template(template)
+        self.default_tpl = Template(template)
+
+        template = """
+upstream {{ domain }} {
+{% for addr in addrs %}
+    server {{ addr }};
+{% endfor %}
+}
+"""
+        self.upstream_tpl = Template(template)
+        template = """
+location {{ path }} {
+    proxy_pass http://{{ domain }};
+    proxy_set_header X-Real-IP $remote_addr;
+}
+"""
+        self.location_tpl = Template(template)
         pass
 
     def init(self):
@@ -45,24 +61,41 @@ server {
 
     def do_router_update(self, tid, msg):
         context = msg['context']
+        domain = context["domain"]
+        path = context.get("path", "")
         dict = {
-            "domain" : context["domain"],
-            "addrs" : context["addrs"]
+            "domain" : domain,
+            "addrs" : context["addrs"],
+            "path" : path
         }
         result = 0
+        
         try:
-            strconf = self.tpl.render(dict)
-            filename = "/letv/nginx/conf.d/%s.conf" % context["domain"]
-            with open(filename, "w") as fd:
-                fd.write(strconf)
-            cmd = "/letv/nginx/nginx reload"
-            ret, _ = util.run_cmd(cmd, out=False)
-            if ret != 0:
-                logging.warning("router_update failed: %d", ret)
-                result = 1
-        except Exception as e:
-            logging.warning("router_update exception: %s", e)
-            result = -1
+            if path != "":
+                strconf = self.upstream_tpl.render(dict)
+                filename = "/letv/nginx/conf.d/%s/upstream-%s.conf" % (domain, path)
+                with open(filename, "w") as fd:
+                    fd.write(strconf)
+
+                strconf = self.location_tpl.render(dict)
+                filename = "/letv/nginx/conf.d/%s/location-%s.conf" % (domain, path)
+                with open(filename, "w") as fd:
+                    fd.write(strconf)
+
+            else:
+                strconf = self.default_tpl.render(dict)
+                filename = "/letv/nginx/conf.d/%s.conf" % domain
+                with open(filename, "w") as fd:
+                    fd.write(strconf)
+                cmd = "/letv/nginx/nginx reload"
+                ret, _ = util.run_cmd(cmd, out=False)
+                if ret != 0:
+                    logging.warning("router_update failed: %d", ret)
+                    result = 1
+            except Exception as e:
+                logging.warning("router_update exception: %s", e)
+                result = -1
+
         reply_msg = {
             'cmd' : 'common_reply',
             'context' : {
@@ -74,10 +107,17 @@ server {
 
     def do_router_remove(self, tid, msg):
         context = msg['context']
+        domain = context["domain"]
+        path = context.get("path", "")
         result = 0
         try:
-            filename = "/letv/nginx/conf.d/%s.conf" % context["domain"]
-            cmd = "rm -f %s && /letv/nginx/nginx reload" % filename
+            if path != "":
+                file1 = "/letv/nginx/conf.d/%s/upstream-%s.conf" % (domain, path)
+                file2 = "/letv/nginx/conf.d/%s/location-%s.conf" % (domain, path)
+                cmd = "rm -f %s && rm -f %s && /letv/nginx/nginx reload" % (file1, file2)
+            else:
+                filename = "/letv/nginx/conf.d/%s.conf" % domain
+                cmd = "rm -f %s && /letv/nginx/nginx reload" % filename
             ret, _ = util.run_cmd(cmd, out=False)
             if ret != 0:
                 logging.warning("router_remove failed: %d", ret)
