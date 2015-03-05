745f64a938df911af0b8b6d092ddb043372bdfb0
chenyf <rstevens2008@hotmail.com>
2015-03-05 14:49:38 +0800

    up

--- scheduler/conf.sh
+++ scheduler/conf.sh
@@ -5,11 +5,16 @@ PYTHON_BIN=python2.6
 
 export AGENT_DIR=/letv/mpass/$AGENT_NAME
 export LOG_DIR=/letv/logs/mpass/$AGENT_NAME/
-export WORK_DIR=/letv/run/mpass/$AGENT_NAME/work
-export STATUS_DIR=/letv/run/mpass/$AGENT_NAME/status
+export RUN_DIR=/letv/run/mpass/$AGENT_NAME
+
+export WORK_DIR=$RUN_DIR/work
+export STATUS_DIR=$RUN_DIR/status
 
 export LOG_FILE=$LOG_DIR/agent.log
+export LOCK_FILE=$RUN_DIR/agent.lock
 export LOG_TYPE="file"
+
+export THIS_HOST=$(grep IPADDR /etc/sysconfig/network-scripts/ifcfg-eth0 | awk 'BEGIN{FS="="} {print $2}')
 
 ## running mode: rdtest, qatest, product
 RUNMODE="product"
--- scheduler/conf/product.conf.sh
+++ scheduler/conf/product.conf.sh
@@ -1,3 +1,7 @@
+export DEBUG_MODE=0
+export HEALTHY_CHECK=1
+export WORKER_NUM=5
+
 export RABBITMQ_SERVER_LIST="10.176.28.145:5672"
 export RABBITMQ_USER="hades_agent"
 export RABBITMQ_PASS="hades_agent"
@@ -5,4 +9,5 @@ export RABBITMQ_PASS="hades_agent"
 export SCHEDULER_VHOST="/hades"
 export SCHEDULER_EXCHANGE_NAME="command.exchange"
 export SCHEDULER_HEARTBEAT_QUEUE="heartbeat.queue"
+export THIS_AGENT_NAME="scheduler@$THIS_HOST"
 
